51412468ba9a9e06bb7f40d604fc91e095c42dcf
bloodteller123 <chenzihan708@gmail.com>
2022-09-26 20:12:28 -0400

    U-TEST Skull-sets related

--- src/test/java/pirate/PlayerTest.java
+++ src/test/java/pirate/PlayerTest.java
@@ -2,6 +2,11 @@ package pirate;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PlayerTest {
@@ -10,5 +15,20 @@ public class PlayerTest {
     void InitializeDicetest() {
         Player p = new Player(0,0);
         assertNotNull(p.getDice());
+    }
+
+    @Test
+    void InitializeSkulls(){
+        Player p = new Player(0,0);
+        assertNotNull(p.getSkulls());
+    }
+    @Test
+    void addSkullsIndicesTest(){
+        Player p = new Player(0,0);
+        ArrayList<Die> dice = new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("skull  "),
+                new Die("skull  "),new Die("skull  "),new Die("skull  "),new Die("skull  "),
+                new Die("skull  "), new Die("skull  ")));
+        addSkulls(dice);
+        assertEquals(8, p.getSkulls().size());
     }
 }
