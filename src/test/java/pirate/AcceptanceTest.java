5339fdbccfa0554079c2098d2a47294fd0c55fa2
bloodteller123 <chenzihan708@gmail.com>
2022-09-30 13:56:00 -0400

    A-TEST 47

--- src/test/java/pirate/AcceptanceTest.java
+++ src/test/java/pirate/AcceptanceTest.java
@@ -101,4 +101,49 @@ public class AcceptanceTest {
         assertEquals(0, p.getScore());
         System.out.println("TEST 46 passed");
     }
+
+    @Test
+    @Order(4)
+    public void Test47(){
+        Player p = new Player(0,0);
+        p.initializeDice();
+        // on first roll
+        p.getDice().forEach(d -> d.roll());
+
+        //1 skull, 4 parrots, 3 swords
+        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("parrot "),
+                new Die("parrot "),new Die("parrot "),new Die("parrot "),new Die("saber  "),
+                new Die("saber  "), new Die("saber  "))));
+
+        p.rerollSome(new String[]{"5", "6", "7"});
+        assertEquals("parrot ", p.getDice().get(1).getFace());
+        assertEquals("parrot ", p.getDice().get(2).getFace());
+        assertEquals("parrot ", p.getDice().get(3).getFace());
+        assertEquals("parrot ", p.getDice().get(4).getFace());
+        p.reset();
+        // 2 skulls 4 parrots 2 monkeys
+        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("parrot "),
+                new Die("parrot "),new Die("parrot "),new Die("parrot "),new Die("skull  "),
+                new Die("monkey "), new Die("monkey "))));
+
+        p.rerollSome(new String[]{"6", "7"});
+        assertEquals("skull  ", p.getDice().get(0).getFace());
+        assertEquals("skull  ", p.getDice().get(5).getFace());
+        assertEquals("parrot ", p.getDice().get(1).getFace());
+        assertEquals("parrot ", p.getDice().get(2).getFace());
+        assertEquals("parrot ", p.getDice().get(3).getFace());
+        assertEquals("parrot ", p.getDice().get(4).getFace());
+        p.reset();
+        // 3 skulls 4 parrots 1 monkeys
+        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("parrot "),
+                new Die("parrot "),new Die("parrot "),new Die("parrot "),new Die("skull  "),
+                new Die("skull  "), new Die("monkey "))));
+        // check # skulls after re roll
+        p.addSkulls(p.getDice());
+        p.skullCheck();
+        assertEquals(3, p.getSkulls().size());
+        assertTrue(p.isEnd());
+        assertEquals(0, p.getScore());
+        System.out.println("TEST 47 passed");
+    }
 }
