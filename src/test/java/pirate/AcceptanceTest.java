7e649db85a04bf43aadfae587aab5e2345134ccf
bloodteller123 <chenzihan708@gmail.com>
2022-09-30 13:37:19 -0400

    A-TEST 46

--- src/test/java/pirate/AcceptanceTest.java
+++ src/test/java/pirate/AcceptanceTest.java
@@ -74,6 +74,31 @@ public class AcceptanceTest {
     @Test
     @Order(3)
     public void Test46(){
-        System.out.println("TEST 46");
+        Player p = new Player(0,0);
+        p.initializeDice();
+        // on first roll
+        p.getDice().forEach(d -> d.roll());
+
+        //2 skull, 4 parrots, 2 swords
+        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("skull  "),
+                new Die("parrot "),new Die("parrot "),new Die("parrot "),new Die("parrot "),
+                new Die("saber  "), new Die("saber  "))));
+
+        p.rerollSome(new String[]{"6", "7"});
+        assertEquals("parrot ", p.getDice().get(5).getFace());
+        assertEquals("parrot ", p.getDice().get(2).getFace());
+        assertEquals("parrot ", p.getDice().get(3).getFace());
+        assertEquals("parrot ", p.getDice().get(4).getFace());
+        p.reset();
+        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("skull  "),
+                new Die("parrot "),new Die("parrot "),new Die("parrot "),new Die("parrot "),
+                new Die("skull  "), new Die("saber  "))));
+        // check # skulls after re roll
+        p.addSkulls(p.getDice());
+        p.skullCheck();
+        assertEquals(3, p.getSkulls().size());
+        assertTrue(p.isEnd());
+        assertEquals(0, p.getScore());
+        System.out.println("TEST 46 passed");
     }
 }
