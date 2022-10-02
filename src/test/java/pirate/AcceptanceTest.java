c1761dbb526ef9b2d72d563fb3f27799e81619d6
bloodteller123 <chenzihan708@gmail.com>
2022-10-01 20:12:25 -0400

    A-TEST test53

--- src/test/java/pirate/AcceptanceTest.java
+++ src/test/java/pirate/AcceptanceTest.java
@@ -211,4 +211,21 @@ public class AcceptanceTest {
         p.calculateScore(dice);
         assertEquals(200, p.getInfo()[0]);
     }
+
+    @Test
+    public void Test53(){
+        Player p = new Player(0,0);
+        p.initializeDice();
+        // on first roll
+        p.getDice().forEach(d -> d.roll());
+        //2 skull, 2 parrots, 2 swords 0 coins 0 diamonds 2 monkeys
+        List<Die> dice = new ArrayList<>(Arrays.asList(new Die("skull  "),new Die("saber  "),
+                new Die("monkey "),new Die("skull  "),new Die("saber  "),new Die("saber  "),
+                new Die("monkey "), new Die("monkey ")));
+        p.setDice(dice);
+        p.setCard("coin");
+        p.calculateScore(dice);
+        assertEquals(300, p.getInfo()[0]);
+    }
+    
 }
