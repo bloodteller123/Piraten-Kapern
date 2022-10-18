78b620801c63a4a066495f2d7ad3cbe585a539f8
bloodteller123 <chenzihan708@gmail.com>
2022-10-17 21:16:00 -0400

    CUCUMBER-TEST row 38

--- src/test/java/pirate/SinglePlayerScoringSDefs.java
+++ src/test/java/pirate/SinglePlayerScoringSDefs.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SinglePlayerScoringSDefs {
     Player p;
-
     @Given("Player is initialized")
     public void playerIsInitialized() {
         p = new Player(0,0);
@@ -22,21 +21,39 @@ public class SinglePlayerScoringSDefs {
     public void diceIsInitialized() {
         p.initializeDice();
     }
-    @When("player rolls {string} {string} and {string} {string}")
-    public void playerRollsAnd(String arg0, String arg1, String arg2, String arg3) {
+    @When("player rolls {int} {string} , {int} {string}")
+    public void playerRolls(int arg0, String arg1, int arg2, String arg3) {
         p.getDice().forEach(d -> d.roll());
         // set to 3 skulls
-        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull  "), new Die("skull  "),
-                new Die("skull  "), new Die("diamond"), new Die("diamond"), new Die("diamond"),
-                new Die("diamond"), new Die("diamond"))));
+        p.setDice(new ArrayList<>(Arrays.asList(new Die(arg1), new Die(arg1),
+                new Die(arg1), new Die(arg3), new Die(arg3), new Die(arg3),
+                new Die(arg3), new Die(arg3))));
     }
-
     @Then("player gets {int} scores")
-    public void playerGetsScores(int arg0) {
+    public void playerGetsScores(int scores) {
         p.addSkulls(p.getDice());
         p.skullCheck();
         assertEquals(3, p.getSkulls().size());
+
+        assertEquals(scores, p.getScore());
+    }
+    @And("player dies")
+    public void playerDies() {
         assertTrue(p.isEnd());
-        assertEquals(0, p.getScore());
+    }
+    @When("player rolls {int} {string} , {int} {string}, {int} {string}")
+    public void playerRolls(int arg0, String arg1, int arg2, String arg3, int arg4, String arg5) {
+        p.getDice().forEach(d -> d.roll());
+        //1 skull, 4 parrots, 3 swords
+        p.setDice(new ArrayList<>(Arrays.asList(new Die(arg1), new Die(arg3),
+                new Die(arg3), new Die(arg3), new Die(arg3), new Die(arg5),
+                new Die(arg5), new Die(arg5))));
+    }
+    @And("player rerolls {int} {string} to get {int} {string}, {int} {string}")
+    public void playerRerollsToGet(int arg0, String arg1, int arg2, String arg3, int arg4, String arg5) {
+        p.rerollSome(new String[]{"5", "6", "7"});
+        p.setDice(new ArrayList<>(Arrays.asList(new Die("skull"), new Die("parrot "),
+                new Die("parrot "), new Die("parrot "), new Die("parrot "), new Die(arg3),
+                new Die(arg3), new Die(arg5))));
     }
 }
--- src/test/resources/SinglePlayerScoring.feature
+++ src/test/resources/SinglePlayerScoring.feature
@@ -4,5 +4,12 @@ Feature: Handle multiple single player scoring scenarios
     And dice is initialized
 
   Scenario: die with 3 skulls 5 swords on first roll and player gets a score of 0
-    When player rolls "3" "skulls" and "5" "sabers"
+    When player rolls 3 "skull" , 5 "saber"
     Then player gets 0 scores
+    And player dies
+
+  Scenario: roll 1 skull, 4 parrots, 3 swords, reroll 3 swords, get 2 skulls 1 sword die
+    When player rolls 1 "skull" , 4 "parrot", 3 "saber"
+    And player rerolls 3 "saber" to get 2 "skull", 1 "saber"
+    Then player gets 0 scores
+    And player dies
