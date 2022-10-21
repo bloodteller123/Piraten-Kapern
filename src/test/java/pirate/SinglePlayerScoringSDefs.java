88a59e57747ba43cf0665fabfb21d559e53e7a20
bloodteller123 <chenzihan708@gmail.com>
2022-10-20 22:50:50 -0400

    [UPDATE] upadte player rolls step definition

--- src/test/java/pirate/SinglePlayerScoringSDefs.java
+++ src/test/java/pirate/SinglePlayerScoringSDefs.java
@@ -1,5 +1,6 @@
 package pirate;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -7,6 +8,8 @@ import io.cucumber.java.en.When;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -21,13 +24,19 @@ public class SinglePlayerScoringSDefs {
     public void diceIsInitialized() {
         p.initializeDice();
     }
-    @When("player rolls {int} {string} , {int} {string}")
-    public void playerRolls(int arg0, String arg1, int arg2, String arg3) {
-        p.getDice().forEach(d -> d.roll());
-        // set to 3 skulls
-        p.setDice(new ArrayList<>(Arrays.asList(new Die(arg1), new Die(arg1),
-                new Die(arg1), new Die(arg3), new Die(arg3), new Die(arg3),
-                new Die(arg3), new Die(arg3))));
+    @When("player rolls")
+    public void playerRolls(DataTable dataTable) {
+        List<Map<String, String>> maps = dataTable.asMaps(String.class, String.class);
+        System.out.println(maps);
+        List<Die> dies = new ArrayList<>();
+        for(Map<String, String> m : maps){
+            int occ = Integer.valueOf(m.get("values"));
+            while(occ >0){
+                dies.add(new Die(m.get("die")));
+                occ--;
+            }
+        }
+        p.setDice(dies);
     }
     @Then("player gets {int} scores")
     public void playerGetsScores(int scores) {
@@ -41,14 +50,6 @@ public class SinglePlayerScoringSDefs {
     public void playerDies() {
         assertTrue(p.isEnd());
     }
-    @When("player rolls {int} {string} , {int} {string}, {int} {string}")
-    public void playerRolls(int arg0, String arg1, int arg2, String arg3, int arg4, String arg5) {
-        p.getDice().forEach(d -> d.roll());
-        //1 skull, 4 parrots, 3 swords
-        p.setDice(new ArrayList<>(Arrays.asList(new Die(arg1), new Die(arg3),
-                new Die(arg3), new Die(arg3), new Die(arg3), new Die(arg5),
-                new Die(arg5), new Die(arg5))));
-    }
     @And("player rerolls {int} {string} to get {int} {string}, {int} {string}")
     public void playerRerollsToGet(int arg0, String arg1, int arg2, String arg3, int arg4, String arg5) {
         p.rerollSome(new String[]{"5", "6", "7"});
@@ -56,4 +57,5 @@ public class SinglePlayerScoringSDefs {
                 new Die("parrot "), new Die("parrot "), new Die("parrot "), new Die(arg3),
                 new Die(arg3), new Die(arg5))));
     }
+
 }
--- src/test/resources/SinglePlayerScoring.feature
+++ src/test/resources/SinglePlayerScoring.feature
@@ -4,12 +4,53 @@ Feature: Handle multiple single player scoring scenarios
     And dice is initialized
 
   Scenario: die with 3 skulls 5 swords on first roll and player gets a score of 0
-    When player rolls 3 "skull" , 5 "saber"
+    When player rolls
+      | die     | values |
+      | diamond | 0      |
+      | skull   | 3      |
+      | saber   | 5      |
+      | parrot  | 0      |
+      | coin    | 0      |
+      | monkey  | 0      |
     Then player gets 0 scores
     And player dies
 
   Scenario: roll 1 skull, 4 parrots, 3 swords, reroll 3 swords, get 2 skulls 1 sword die
-    When player rolls 1 "skull" , 4 "parrot", 3 "saber"
+    When player rolls
+      | die     | values |
+      | diamond | 0      |
+      | skull   | 1      |
+      | saber   | 3      |
+      | parrot  | 4      |
+      | coin    | 0      |
+      | monkey  | 0      |
     And player rerolls 3 "saber" to get 2 "skull", 1 "saber"
+    Then player gets 0 scores
+    And player dies
+
+  Scenario: roll 1 skull, 4 parrots, 3 swords, reroll 3 swords, get 2 skulls 1 sword die
+    When player rolls
+      | die     | values |
+      | diamond | 0      |
+      | skull   | 2      |
+      | saber   | 2      |
+      | parrot  | 4      |
+      | coin    | 0      |
+      | monkey  | 0      |
+    And player rerolls 2 "saber" to get 1 "skull", 1 "saber"
+    Then player gets 0 scores
+    And player dies
+
+  Scenario: roll 1 skull, 4 parrots, 3 swords, reroll 3 swords, get 2 skulls 1 sword die
+    When player rolls
+      | die     | values |
+      | diamond | 0      |
+      | skull   | 1      |
+      | saber   | 3      |
+      | parrot  | 4      |
+      | coin    | 0      |
+      | monkey  | 0      |
+    And player rerolls 3 "saber" to get 1 "skull", 2 "monkey"
+    And player rerolls 2 "monkey" to get 1 "skull", 1 "monkey"
     Then player gets 0 scores
     And player dies
