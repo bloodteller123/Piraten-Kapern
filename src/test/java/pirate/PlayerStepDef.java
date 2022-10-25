f9c18ed8dba7dd443a809eac5ad6d9334c6dada7
bloodteller123 <chenzihan708@gmail.com>
2022-10-24 23:04:04 -0400

    Test 122

--- src/test/java/pirate/PlayerStepDef.java
+++ src/test/java/pirate/PlayerStepDef.java
@@ -21,13 +21,13 @@ public class PlayerStepDef {
             ps[i] = new Player(i+1,0);
         }
     }
-    public void reroll_logic(List<Map<String, String>> maps, List<Die> list, Set<Integer> set, String arg0){
+    public void reroll_logic(List<Map<String, String>> maps, List<Die> list, Set<Integer> set, String s){
         for(int i=0;i<maps.size();i++){
             int occ = Integer.parseInt(maps.get(i).get("values"));
             String die_val = maps.get(i).get("die");
             while(occ >0){
                 int index = IntStream.range(0, list.size())
-                        .filter(j -> list.get(j).getFace().trim().equals(arg0) && !set.contains(j))
+                        .filter(j -> list.get(j).getFace().trim().equals(s) && !set.contains(j))
                         .findFirst()
                         .orElse(-1);
                 if(index!=-1) {
@@ -62,11 +62,11 @@ public class PlayerStepDef {
 
     @When("player {int} has fortunate card {string}")
     public void playerHasFortunateCard(int arg0, String arg1) {
-        ps[arg0].setCard(arg1);
+        ps[arg0-1].setCard(arg1);
         if(arg1.contains("sword")) {
-            ps[arg0].activateSeaBattles(Integer.parseInt(arg1.split("-")[0]));
+            ps[arg0-1].activateSeaBattles(Integer.parseInt(arg1.split("-")[0]));
         }else if(arg1.contains("skull")) {
-            ps[arg0].setSkulls(arg1.split("-"));
+            ps[arg0-1].setSkulls(arg1.split("-"));
         }
     }
 
@@ -81,42 +81,44 @@ public class PlayerStepDef {
                 occ--;
             }
         }
-        ps[arg0].setDice(dies);
+        ps[arg0-1].setDice(dies);
     }
 
     @And("player {int} rerolls {string} to get")
     public void playerRerollsToGet(int arg0, String arg1, DataTable dataTable) {
         List<Map<String, String>> maps = dataTable.asMaps(String.class, String.class);
         String[] ind = new String[maps.size()];
-        List<Die> list = ps[arg0].getDice();
+        List<Die> list = ps[arg0-1].getDice();
         Set<Integer> set = new HashSet<>();
         reroll_logic(maps, list, set, arg1);
-        ps[arg0].setDice(list);
-        System.out.println(ps[arg0].getDice());
+        ps[arg0-1].setDice(list);
+        System.out.println(ps[arg0-1].getDice());
     }
 
     @Then("player {int} does skull check")
     public void playerDoesSkullCheck(int arg0) {
-        ps[arg0].addSkulls(ps[arg0].getDice());
-        ps[arg0].skullCheck(false);
+        ps[arg0-1].addSkulls(ps[arg0-1].getDice());
+        ps[arg0-1].skullCheck(false);
     }
     @And("player {int} gets {int} scores")
     public void playerGetsScores(int arg0, int scores) {
-        if(!(ps[arg0].getSkullSize() >=3)){
-            System.out.println(ps[arg0].getCard());
-            ps[arg0].calculateScore(ps[arg0].getDice());
-            assertEquals(scores, ps[arg0].getInfo()[0]);
+        System.out.println("player " +arg0);
+        if(!(ps[arg0-1].getSkullSize() >=3)){
+            System.out.println(ps[arg0-1].getCard());
+            System.out.println(ps[arg0-1].getDice());
+            ps[arg0-1].calculateScore(ps[arg0-1].getDice());
+            assertEquals(scores, ps[arg0-1].getInfo()[0]);
         }else{
             System.out.println("ELSE");
-            ps[arg0].calculateScore(ps[arg0].getDice());
-            System.out.println(ps[arg0].getInfo()[0] + " "+ps[arg0].getInfo()[1]);
+            ps[arg0-1].calculateScore(ps[arg0-1].getDice());
+            System.out.println(ps[arg0-1].getInfo()[0] + " "+ps[arg0-1].getInfo()[1]);
         }
     }
 
     @And("player {int} dies")
     public void playerDies(int arg0) {
-        assertTrue(ps[arg0].getSkullSize() >=3);
-        assertTrue(ps[arg0].isEnd());
+        assertTrue(ps[arg0-1].getSkullSize() >=3);
+        assertTrue(ps[arg0-1].isEnd());
     }
 
     @And("dice are initialized")
@@ -129,40 +131,46 @@ public class PlayerStepDef {
 
     @And("player {int} deducts {int} points")
     public void playerDeductsPoints(int arg0, int deduction) {
-        ps[arg0].calculateScore(ps[arg0].getDice());
-        assertEquals(deduction, ps[arg0].getInfo()[1]);
+        ps[arg0-1].calculateScore(ps[arg0-1].getDice());
+        assertEquals(deduction, ps[arg0-1].getInfo()[1]);
     }
 
     @And("player {int} enters islandofskulls")
     public void playerEntersIslandofskulls(int arg0) {
-        System.out.println(ps[arg0].getDice());
-        assertTrue(ps[arg0].getSkullSize() >3);
+        System.out.println(ps[arg0-1].getDice());
+        assertTrue(ps[arg0-1].getSkullSize() >3);
     }
 
     @And("player {int} puts dice in chest")
     public void playerPutsDiceInChest(int arg0,Map<String, String> map) {
         int total = map.values().stream().mapToInt(Integer::parseInt).sum();
-        List<Die> l = ps[arg0].getDice();
+        List<Die> l = ps[arg0-1].getDice();
         String[] inds = buildIndArray(total, map,l);
-        ps[arg0].addToTreasures(inds);
+        ps[arg0-1].addToTreasures(inds);
     }
 
     @And("player {int} takes out dice from chest")
     public void playerTakesOutDiceFromChest(int arg0, Map<String, String> map) {
         int total = map.values().stream().mapToInt(Integer::parseInt).sum();
-        List<Die> l = ps[arg0].getDice();
+        List<Die> l = ps[arg0-1].getDice();
         String[] inds = buildIndArray(total, map,l);
-        ps[arg0].removeFromTreasures(inds);
+        ps[arg0-1].removeFromTreasures(inds);
     }
 
     @And("player {int} makes {int} deduction for other player")
     public void playerMakesDeductionForOtherPlayer(int arg0, int arg1) {
-        ps[arg0].deductPoints(ps[arg0].getSkullSize());
-        assertEquals((arg1 *= -1), ps[arg0].getInfo()[1]);
+        ps[arg0-1].deductPoints(ps[arg0-1].getSkullSize());
+        assertEquals((arg1 *= -1), ps[arg0-1].getInfo()[1]);
     }
 
     @And("player {int} round ends")
     public void playerRoundEnds(int arg0) {
-        assertTrue(ps[arg0].isEnd());
+        assertTrue(ps[arg0-1].isEnd());
+    }
+
+    @Then("player {int} wins")
+    public void playerWins(int arg0) {
+        List<Integer> l = Arrays.asList(ps[0].getInfo()[0],ps[1].getInfo()[0],ps[2].getInfo()[0]);
+        assertEquals(arg0-1, l.indexOf(Collections.max(l)));
     }
 }
--- src/test/resources/PartThree.feature
+++ src/test/resources/PartThree.feature
@@ -2,7 +2,7 @@ Feature: Handle cases with different players
   Background:
     Given players are initialized
     And dice are initialized
-  Scenario: player1 rolls 7 swords + 1 skull with FC captain (gets 4000 points - could win)
+  Scenario: row 122 player1 rolls 7 swords + 1 skull with FC captain (gets 4000 points - could win)
   then player2 rolls 7 swords 1 skull  with FC 1 skull (2000)
   then player3 scores 0 (3 skulls, 5 monkeys, FC coin) => game stops and declares player 1 wins
     When player 1 has fortunate card "captain"
@@ -14,3 +14,29 @@ Feature: Handle cases with different players
       | parrot  | 0      |
       | coin    | 0      |
       | monkey  | 0      |
+    And player 1 does skull check
+    And player 1 gets 4000 scores
+    And player 2 has fortunate card "1-skull"
+    And player 2 rolls
+      | die     | values |
+      | diamond | 0      |
+      | skull   | 1      |
+      | saber   | 7      |
+      | parrot  | 0      |
+      | coin    | 0      |
+      | monkey  | 0      |
+    And player 2 does skull check
+    And player 2 gets 2000 scores
+    And player 3 has fortunate card "coin"
+    And player 3 rolls
+      | die     | values |
+      | diamond | 0      |
+      | skull   | 3      |
+      | saber   | 0      |
+      | parrot  | 0      |
+      | coin    | 0      |
+      | monkey  | 5      |
+    And player 3 does skull check
+    And player 3 gets 0 scores
+    And player 3 dies
+    Then player 1 wins
